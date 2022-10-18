e81a48139c262ae7635b2a713b46471d3cffe201
Kenny W. Lino <kennywlino@gmail.com>
2022-10-18 16:49:47 -0700

    Removing unnecessary imports.

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 class HornedBeast extends React.Component {
     constructor(props) {
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import HornedBeast from './HornedBeast.js';
 import data from '../assets/data.json';
